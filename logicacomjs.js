682f6799017938c4b6046e9091210c38f8b11a76
Miguel1980-Git <miguelmpassos1980@gmail.com>
2025-05-22 17:06:43 -0300

    aprendendo prompt

---
+++ logicacomjs.js
@@ -0,0 +1,5 @@
+console,log(x)
+let salario = Number(prompt("Digite seu salario:"))
+let mestrado = Number(prompt("Digite quantos meses voce trabalhou:"))
+let decimo = salario/12*mestrado
+alert("Seu décimo é: R$" + decimo)
